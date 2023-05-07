2dc77ad6574398c0cccdd47453f60a4b74bdb948
MichalZaborowski91 <mkzaborowski@gmail.com>
2023-05-07 13:42:34 +0100

    first init

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
